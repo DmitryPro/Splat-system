91db74c61f856d3e4ce40b07f24753a010d97633
Freemahn <gordon.pawel@yandex.ru>
2015-08-22 22:13:20 +0300

    Tomcat Simple server

---
+++ Server/src/main/java/HelloServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Freemahn on 21.08.2015.
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/index.html");
+    }
+}
---
+++ Server/src/main/java/SendServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * Created by Freemahn on 21.08.2015.
+ */
+public class SendServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Socket socket = new Socket("198.199.73.244", 8080);
+        socket.getOutputStream().write("hello".getBytes());
+        //socket.connect(new InetSocketAddress("198.199.73.244", 8080));
+        String answer = "";
+        while (socket.getInputStream().available() >= 0) {
+            answer += (char) socket.getInputStream().read();
+        }
+        resp.getOutputStream().print(answer);
+    }
+}
